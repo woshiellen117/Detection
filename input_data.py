581aa64135fad294215f6948dbc242f59f2e96b2
woshiellen117 <woshiellen2@163.com>
2018-01-12 19:09:04 +0800

    2

--- input_data.py
+++ input_data.py
@@ -1,13 +1,34 @@
 import skimage.io as io
 import cv2
+from skimage import data_dir
+import tensorflow as tf
 
-file_path = "D:/final/try/*.png"
-file_path = "D:\\final\\thumb\\00000000.png"
+# file_path = "D:/final/try/*.png"
+file_path = "D:\\final\\try_100"
 # file_path = "1.TIF"
 
-image = cv2.imread(file_path)
+# #
+# image=cv2.imread(file_path)
+# io.imshow(image)
 
+# collection=io.imread_collection(file_path)
+# # # print(len(collection))
+# i=0
+# for i in range(100):
+#     image=cv2.imread(file_path)
+#     io.imshow(image)
+str=file_path + '/*.png'
+coll = io.ImageCollection(str)
+print(len(coll))
 
-collection=io.imread_collection(file_path)
-print(len(collection))
-io.imshow(collection[20])
+x = tf.Variable(coll[20],name='x')
+model = tf.initialize_all_variables()
+
+with tf.Session() as session:
+    x = tf.transpose(x,perm=[1,0])
+    session.run(model)
+    result = session.run(x)
+
+cv2.namedWindow('image',0)
+cv2.imshow('image',coll[20])
+cv2.waitKey(0)
